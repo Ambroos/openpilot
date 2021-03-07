7d79aa1305007c020089eeb5a5ec26f1d3501990
Vehicle Researcher <user@comma.ai>
2021-03-07 15:44:24 -0800

    openpilot v0.8.3 release

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -1,23 +1,22 @@
-import os
-
 from selfdrive.manager.process import PythonProcess, NativeProcess, DaemonProcess
 from selfdrive.hardware import EON, TICI, PC
-
-WEBCAM = os.getenv("WEBCAM") is not None
 
 procs = [
   DaemonProcess("manage_athenad", "selfdrive.athena.manage_athenad", "AthenadPid"),
   # due to qualcomm kernel bugs SIGKILLing camerad sometimes causes page table corruption
   NativeProcess("camerad", "selfdrive/camerad", ["./camerad"], unkillable=True, driverview=True),
   NativeProcess("clocksd", "selfdrive/clocksd", ["./clocksd"]),
+  NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], driverview=True),
   NativeProcess("logcatd", "selfdrive/logcatd", ["./logcatd"]),
   NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"]),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"]),
   NativeProcess("proclogd", "selfdrive/proclogd", ["./proclogd"]),
+  NativeProcess("ubloxd", "selfdrive/locationd", ["./ubloxd"]),
   NativeProcess("ui", "selfdrive/ui", ["./ui"], persistent=True),
   PythonProcess("calibrationd", "selfdrive.locationd.calibrationd"),
   PythonProcess("controlsd", "selfdrive.controls.controlsd"),
   PythonProcess("deleter", "selfdrive.loggerd.deleter", persistent=True),
+  PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", driverview=True),
   PythonProcess("locationd", "selfdrive.locationd.locationd"),
   PythonProcess("logmessaged", "selfdrive.logmessaged", persistent=True),
   PythonProcess("pandad", "selfdrive.pandad", persistent=True),
@@ -33,13 +32,6 @@ if not PC:
     NativeProcess("sensord", "selfdrive/sensord", ["./sensord"], persistent=EON, sigkill=EON),
     PythonProcess("tombstoned", "selfdrive.tombstoned", persistent=True),
     PythonProcess("updated", "selfdrive.updated", persistent=True),
-  ]
-
-if not PC or WEBCAM:
-  procs += [
-    NativeProcess("ubloxd", "selfdrive/locationd", ["./ubloxd"]),
-    PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", driverview=True),
-    NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], driverview=True),
   ]
 
 if TICI:
